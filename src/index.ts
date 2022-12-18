5c767e80a39fab62d1b1287194a1ed15f98a1b38
Adrian Vincent Aguilar <aguilaradrianvincent@gmail.com>
2022-12-18 21:48:54 +0800

    feat: add many to many broadcasting

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,57 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import cors from 'cors';
-import { roomHandler } from './room';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
-const app = express();
-app.use(cors);
+let app = express();
+app.use(cors());
 const port = 8080;
 const server = http.createServer(app);
 
 // eslint-disable-next-line no-use-before-define
-app.route('/get').get((req, res) => {
-  const re = req.query.params;
-  console.log(re);
-  res.send('hello');
+app.route("/get").get((req, res) => {
+	const re = req.query.params;
+	console.log(re);
+	res.send("hello");
 });
 
+// const io = new Server(server, { cors: { origin: "*", credentials: true } });
 const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
+	cors: {
+		origin: "*"
+		// methods: ["GET", "POST"]
+	}
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  roomHandler(socket);
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
+let broadcaster = "";
 
-server.listen(port, () => {
-  console.log(`listening on *:${port}`);
+io.sockets.on("error", (e) => console.log(e));
+io.sockets.on("connection", (socket) => {
+	console.log("server connection", socket.id);
+	socket.on("broadcaster", (roomid) => {
+		console.log("server broadcast", socket.id);
+		broadcaster = socket.id;
+		socket.broadcast.emit("broadcaster", socket.id, roomid);
+	});
+	socket.on("watcher", (broadcaster) => {
+		console.log("server watcher", socket.id);
+		socket.to(broadcaster).emit("watcher", socket.id);
+	});
+	socket.on("offer", (id, message) => {
+		console.log("server offer", socket.id);
+		socket.to(id).emit("offer", socket.id, message);
+	});
+	socket.on("answer", (id, message) => {
+		console.log("server answer", socket.id);
+		socket.to(id).emit("answer", socket.id, message);
+	});
+	socket.on("candidate", (id, message) => {
+		console.log("server candidate", socket.id);
+		socket.to(id).emit("candidate", socket.id, message);
+	});
+	socket.on("disconnect", () => {
+		console.log("server disconnect", socket.id);
+		socket.to(broadcaster).emit("disconnectPeer", socket.id);
+	});
 });
-
+server.listen(port, () => console.log(`Server is running on port ${port}`));
